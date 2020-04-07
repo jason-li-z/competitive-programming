482365c56fd5023e13de15958b18596bd6e31ad4
jason-li-z <jiceredits@gmail.com>
2020-04-07 16:37:48 -0700

    Added recursive solution, both runtime & space are O(N)

--- Leetcode/Jump Game III.cpp
+++ Leetcode/Jump Game III.cpp
@@ -38,4 +38,31 @@ class Solution {
         }
         return false;
     }
+
+    bool canReachRecursive(vector<int> &arr, int start) {
+        const int n = arr.size();
+        vector<bool> seen(n);
+        return helper(arr, seen, n, start);
+    }
+
+    bool helper(vector<int> &arr, vector<bool> &seen, int size, int index) {
+        // Out of bounds checks, and base case
+        if (index < 0 || index >= size || seen[index]) {
+            return false;
+        }
+
+        if (arr[index] == 0) {  // If condition met (val == 0)
+            return true;
+        }
+        seen[index] = true;
+
+        if (helper(arr, seen, size, index + arr[index])) {  // Recurse plus, and minus
+            return true;
+        }
+        if (helper(arr, seen, size, index - arr[index])) {
+            return true;
+        }
+
+        return false;  // if doesn't hit recurse return case, we can simply return false
+    }
 };
