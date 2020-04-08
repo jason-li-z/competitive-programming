5774fdac40c958237451a5916f7a3a98e7d4faef
jason-li-z <jiceredits@gmail.com>
2020-04-08 01:18:01 -0700

    Leetcode - Middle of Linked List
    2 ways; using array to store nodes - O(N) space & time, using 2 pointers O(N) time O(1) space

---
+++ Leetcode/30-Day/Middle of Linked List.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        if(head == nullptr) {
+            return head;
+        }
+        
+        ListNode* slow = head;
+        ListNode* fast = head;
+        
+        while(fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        
+        return slow;
+    }
+};
